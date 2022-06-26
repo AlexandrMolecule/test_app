1431a1ca7eec76c4859dd3b1dc881b4cd6388352
Aleksandr <sanyamolekula@gmail.com>
2022-06-26 14:24:03 +0300

    remove prints

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,5 @@ import 'package:test_app/src/feature/weather/data/weather_data_provider.dart';
 void main() async {
   const app = App();
   runApp(app);
-  // final response = await GeolocationDataProviderImpl(client: NetworkClient())
-  //     .searchLocation(queryLocation: "Moscow");
-  final response = await WeatherDataProviderImpl(client: NetworkClient())
-      .getWeatherForecast(lon: -94.04, lat: 33.44);
-  print(response.first.feelsLike.night);
+
 }
